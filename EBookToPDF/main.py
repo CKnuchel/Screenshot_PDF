d08065fe47306165f07c00ba444824a6ea23b1e6
Christoph Knuchel <119663666+CKnuchel@users.noreply.github.com>
2023-07-13 14:30:59 +0200

    Update main.py

--- EBookToPDF/main.py
+++ EBookToPDF/main.py
@@ -11,39 +11,33 @@ from ocr import *
 
 fill = "------------------------------------------------------------------------------------------------------------------------"
 
-print("Programm gestartet")
+print("Start...")
 print(fill)
-print("Wo soll die Datei erstellt werden?")
+print("Where should the file be created?")
 
 target = pathSelection()
 screenshotFolder = target
 screenshotFolderString = str(screenshotFolder)
 screenshotCount = 0
 
-pages = input("Wie viele Seiten hat das Dokument? ")
-ausgabeName = input("Wie soll die Datei heissen? (ohne Leerschläge und Endung) ")
+pages = input("How many pages does the document have? ")
+ausgabeName = input("What should the file be called? (without spaces and extension) ")
 print(fill)
 
-korr = input("Möchtest du die Koordinaten manuell eingeben? (j/n) ").lower()
 
-if korr == "j":
-    x = input("x (obere linke Ecke): ")
-    y = input("y (obere linke Ecke): ")
-    b = input("x (obere rechte Ecke): ")
-    h = input("y (untere linke Ecke): ")
-    width = int(b) - int(x)
-    height = int(h) - int(y)
-else:
-    x = 1080 #Links oben x
-    y = 380 #Links oben y
-    height = 1640 #Höhe ab x
-    width = 1170 #Breite ab y
+
+x = input("x (upper left corner): ")
+y = input("y (upper left corner): ")
+b = input("x (upper right corner): ")
+h = input("y (lower left corner): ")
+width = int(b) - int(x)
+height = int(h) - int(y)
 
 def screenshot(selection):
     screenshot = pyautogui.screenshot(region=(x, y, width, height))
     screenshot.save(target + "/screenshot" + str(screenshotCount) + ".png")
 
-print("In 5 Sekunden beginnt es, wechsle zum Bilschirm, den du aufnehmen möchtest")
+print("In 5 seconds it starts, switch to the screen you want to capture")
 print(fill)
 time.sleep(5)
 
@@ -53,21 +47,21 @@ while screenshotCount < int(pages):
     screenshotCount += 1
     time.sleep(1)
 
-print("Möchtest du ein PDF erstellen? (j/n)")
+print("Do you want to create a PDF? (y/n)")
 pdf = input().lower()
 
-if pdf == "j":
-    print("Möchtest du OCR anwenden? (j/n)")
+if pdf == "j" or pdf == "y":
+    print("Do you want to use OCR? (y/n)")
     ocr = input().lower()
     print(fill)
 
-if pdf == "j" and ocr == "n":
+if pdf == "j" or pdf == "y" and ocr == "n":
     pdf = fpdf.FPDF()
     for i in range(0, screenshotCount):
         pdf.add_page()
         pdf.image(target + "/screenshot" + str(i) + ".png", 0, 0, 210, 297)
     pdf.output(target + "/" + ausgabeName + ".pdf", "F")
-    print("PDF erstellt")
+    print("PDF created")
 
     #Löschen der Screenshots auf Windows
     if os.name == "nt":
@@ -79,13 +73,14 @@ if pdf == "j" and ocr == "n":
         for i in range(0, screenshotCount):
             os.system("rm " + target + "/screenshot" + str(i) + ".png")
 
-    print("Screenshots gelöscht")
+    print("Screenshots deleted")
     
-if pdf == "j" and ocr == "j": 
+if pdf == "y" or pdf == "j" and ocr == "j" or ocr == "y": 
     try:
         ocr_funktion(screenshotFolderString, screenshotCount, ausgabeName)
     except:
-        print("Stelle sicher, das Tesseract installiert ist oder frag Chrigu :)")
+        print("Make sure that Tesseract is installed")
+        print("")
     
 else:
     print("PDF nicht erstellt")
